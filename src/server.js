d3b3917bbfbfc83e66bf5b1be495c55ece733793
afpp3 <afelipp3@gmail.com>
2020-10-04 18:59:50 -0300

    Simple Portfólio

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const favicon = require('serve-favicon');
 
 const server = express()
 
 server.use(express.static('public'))
-server.use(favicon(__dirname + '/public/img/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 
 server.set('view engine', "html")
